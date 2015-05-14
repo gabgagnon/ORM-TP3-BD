38c6d3abe52cc4e884cdb957bb3be9ef40c93da0
gabgagnon <gabgagnon1995@gmail.com>
2015-05-13 20:23:13 -0400

    okokook

--- Application Console/ConsoleApplication1/Program.cs
+++ Application Console/ConsoleApplication1/Program.cs
@@ -21,6 +21,7 @@ namespace ConsoleApplication1
             init.InitializeDatabase(context);
             SectionA(context);
             SectionB(context);
+            SectionC(context);
         }
 
         public static void SectionA(ProjetContext appContext)
@@ -91,28 +92,81 @@ namespace ConsoleApplication1
                 Console.WriteLine("{0}, {1}, {2}, {3}", "|" + tutorsItr.NomFamilleTutor, "|" + tutorsItr.PrenomTutor, "|" + tutorsItr.EmailTutor, "|" + tutorsItr.TutoringTime + "h");
             }
 
-
             DateTime TODAY = DateTime.Now;
             var TutorsAfterToday = appContext.Tutor
-                  .GroupJoin(appContext.TutoringSession,
+                  .Join(appContext.TutoringSession,
                    tutorTable => tutorTable.Id,
                    tutoringSessionTable => tutoringSessionTable.TutorId,
                     (tutorTable, tutoringSessionTable) => new
                     {
                         NomFamilleTutor = tutorTable.LastName,
                         PrenomTutor = tutorTable.FirstName,
-                        EmailTutor = tutorTable.EmailAddress,
-                        TutoringTime = tutoringSessionTable.Sum(s => (int?)s.LengthSession) ?? 0,
-                        LastDate = tutoringSessionTable.Where(d => (int?)d.DateSession.CompareTo(TODAY))
-                        
-                    }).Where(where => where.LastDate < 0);
+                        LastDate = tutoringSessionTable.DateSession.CompareTo(TODAY)
+
+                    });
             Console.WriteLine();
             Console.WriteLine("TUTOR LIST WITH HOURS:");
-            foreach (var tutorsItr in tutors)
+            foreach (var tutorsItr in TutorsAfterToday)
             {
-                Console.WriteLine("{0}, {1}, {2}, {3}", "|" + tutorsItr.NomFamilleTutor, "|" + tutorsItr.PrenomTutor, "|" + tutorsItr.EmailTutor, "|" + tutorsItr.TutoringTime + "h");
+                if (tutorsItr.LastDate <= 0)
+                    Console.WriteLine("{0}, {1}", "|" + tutorsItr.NomFamilleTutor, "|" + tutorsItr.PrenomTutor);
             }
 
+            Console.ReadLine();
+
+
+        }
+
+        
+        protected static void SectionC(ProjetContext appContext)
+        {
+            Tutor tutorToUpgrade;
+            tutorToUpgrade = appContext.Tutor.Where(t => t.FirstName == "Gary" && t.LastName == "Bilodeau").FirstOrDefault<Tutor>();
+
+            if (tutorToUpgrade != null)
+            {
+                tutorToUpgrade.EmailAddress = "gbilodeau@invalidemail.com";
+            }
+
+
+            HelpedStudent helpedStudentToUpgrade;
+            helpedStudentToUpgrade = appContext.HelpedStudent.Where(hs => hs.FirstName == "Marc" && hs.LastName == "Arsenault").FirstOrDefault<HelpedStudent>();
+            TutoringSession TutoringSessionOfHelpedStudentToUpgrade;
+            TutoringSessionOfHelpedStudentToUpgrade = appContext.TutoringSession.Where(s => s.Helped.Id == helpedStudentToUpgrade.Id).FirstOrDefault();
+            appContext.TutoringSession.Remove(TutoringSessionOfHelpedStudentToUpgrade);
+            appContext.HelpedStudent.Remove(helpedStudentToUpgrade);
+            appContext.SaveChanges();
+
+
+            HelpedStudent helpedStudentToUpgrade02;
+            helpedStudentToUpgrade02 = appContext.HelpedStudent.Where(hs => hs.FirstName == "Marc" && hs.LastName == "Arsenault").FirstOrDefault<HelpedStudent>();
+            TutoringSession TutoringSessionToUpgrade;
+            TutoringSessionToUpgrade = appContext.TutoringSession.Where(ts => ts.Helped.Id == helpedStudentToUpgrade02.Id).FirstOrDefault<TutoringSession>();
+
+            if (tutorToUpgrade != null)
+            {
+                TutoringSessionToUpgrade.DateSession = new DateTime(2015, 04, 09, 11, 0, 0);
+                TutoringSessionToUpgrade.TimeSession = 2;
+            }
+            appContext.SaveChanges();
+
+            HelpedStudent helpedStudentToJoin;
+            helpedStudentToJoin = appContext.HelpedStudent.Where(hs => hs.FirstName == "Samuel" && hs.LastName == "Vachon").FirstOrDefault<HelpedStudent>();
+            Tutor TutorToJoin;
+            TutorToJoin = appContext.Tutor.Where(hs => hs.FirstName == "Louis" && hs.LastName == "Vézina").FirstOrDefault<Tutor>();
+            TutoringSession newTutoringSession = new TutoringSession()
+            {
+                DateSession = new DateTime(2015, 06, 04, 10, 0, 0),
+                TimeSession = 10,
+                LengthSession = 2,
+                Helped = helpedStudentToJoin,
+                Tutor = TutorToJoin
+            };
+            appContext.TutoringSession.Add(newTutoringSession);
+            appContext.SaveChanges();
         }
     }
+
+
+
 }
