555baed9b5345c0d2b794ca0c15573d1b647f53c
gabgagnon <gabgagnon1995@gmail.com>
2015-05-18 16:54:42 -0400

    tayeul

--- Application Console/ConsoleApplication1/TutorProgramRepository/SinapseRepository.cs
+++ Application Console/ConsoleApplication1/TutorProgramRepository/SinapseRepository.cs
@@ -9,23 +9,26 @@ namespace ConsoleApplication1.Repository
     class SinapseRepositorySinapseRepository<T> : IEntityRepository<T> where T : Entity
     {
 
-    protected readonly ProjectContext context;
+    protected readonly ProjectContext sinapseContext;
     public SinapseRepository (ProjectContext sinapseContext)
     {
-    this.context = sinapseContext;
+    this.sinapseContext = sinapseContext;
     //Database.setInitializer<SipnapseContext>(initializer);
         // initializer.initializeDatabase(context);
     }
     public IQueryable<T> GetAll()
     {
-     // return SinapseContext.Set<T>().AsQueryable();
+     return sinapseContext.Set<T>().AsQueryable();
     }
     public T GetById(int id)
-    {}
+    {
+    }
     public void DeleteAll()
-    {}
+    {
+    }
     public void Delete(T entity)
-    {}
+    {
+    }
     public void Add(T entity)
     {
     //SinapseContext.
---
+++ Application Console/ConsoleApplication1/TutorProgramRepository/TutorProgramApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.TutorProgramRepository
+{
+    class TutorProgramApplication (ProjectContext context)
+    {
+        private sinapseRepository<Employe> employes;
+        private sinapseRepository<Tutor> tutors;
+        private sinapseRepository<TutoringSession> tutoringSessions;
+
+        /* employes.Insert(emp1) == appContext.Employes.Add(emp3)
+         * var emps = employes.GetAll()
+         */
+    }
+}
